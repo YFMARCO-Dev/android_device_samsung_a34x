bf36dbe9b07b565d360c827f119a8a3431ad7a61
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2026-08-28 00:56:19 +0300

    fix build

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 The TWRP Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-export ALLOW_MISSING_DEPENDENCIES=true
